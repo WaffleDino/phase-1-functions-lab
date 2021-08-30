cd8e141e60e0551d2a94a75ff2cce8a65d4148f6
Jesse <jdnely@gmail.com>
2021-08-30 15:51:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const hq = 42;
+let start;
+let destination;
+const distanceInFeet = (start - destination)*264
+
+
+function distanceFromHqInBlocks(start){
+    return Math.abs(hq - start);
+}
+
+function distanceFromHqInFeet(start) {
+    return Math.abs((hq - start) * 264);
+}
+function distanceTravelledInFeet(start,destination) {
+    return Math.abs((start - destination) * 264);
+}
+
+function calculatesFarePrice(start,destination) {
+    let distanceInFeet = distanceTravelledInFeet(start,destination);
+    if ((distanceInFeet >=400) && (distanceInFeet <=2000)) {
+        return (distanceInFeet - 400) *.02;
+    }
+    else if ((distanceInFeet >2000) && (distanceInFeet <=2500)) {
+        return 25
+    }
+    else if (distanceInFeet > 2500) {
+        return 'cannot travel that far';
+    }
+    else {
+        return 0;
+    }
+}
